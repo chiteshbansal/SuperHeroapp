f3d115da72df48d26751745f93ced6a1841a9a12
chitesh <chiteshbansal111@gmail.com>
2020-08-23 16:33:36 +0530

    Updates

---
+++ precache-manifest.a9e8f332162e97da33ecec6d6506da8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06050b92dffa822b2d4d617b877dfd4b",
+    "url": "/SuperHeroapp/index.html"
+  },
+  {
+    "revision": "f1409cc649f3efe050c7",
+    "url": "/SuperHeroapp/static/css/main.f40c7de9.chunk.css"
+  },
+  {
+    "revision": "40b7992eb6ad929e9210",
+    "url": "/SuperHeroapp/static/js/2.1bc75d9d.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/SuperHeroapp/static/js/2.1bc75d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1409cc649f3efe050c7",
+    "url": "/SuperHeroapp/static/js/main.c3e4d3a9.chunk.js"
+  },
+  {
+    "revision": "1e1bd484523d6e519480",
+    "url": "/SuperHeroapp/static/js/runtime-main.7a060add.js"
+  }
+]);
